c6aa8dab11735c42d929b6223f8d478b2ea02d1d
Anoop H.N <anoop.hn.ext@wsa.com>
2021-10-07 17:19:10 +0530

    Global project

---
+++ FirstApp.Global/FirstAppBase.cs
@@ -0,0 +1,17 @@
+﻿using SimpleInjector;
+using SQLite;
+using System;
+using System.IO;
+
+namespace FirstApp.Global
+{
+    public class FirstAppBase
+    {
+        public static Container Container { get; }
+
+        static FirstAppBase()
+        {
+            Container = new Container();
+        }
+    }
+}
