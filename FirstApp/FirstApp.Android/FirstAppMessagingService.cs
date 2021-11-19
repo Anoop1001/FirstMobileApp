bedb7f08a0d9fa4f8c0022917ccd720b0a979d8a
Anoop H.N <anoop.hn.ext@wsa.com>
2021-11-19 17:04:13 +0530

    Implementation of Firebase cloud messaging

---
+++ FirstApp/FirstApp.Android/FirstAppMessagingService.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Firebase.Messaging;
+
+namespace FirstApp.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirstAppMessagingService : FirebaseMessagingService
+    {
+        public override void OnMessageReceived(RemoteMessage p0)
+        {
+            base.OnMessageReceived(p0);
+        }
+
+        public override void OnNewToken(string p0)
+        {
+            base.OnNewToken(p0);
+        }
+    }
+}
